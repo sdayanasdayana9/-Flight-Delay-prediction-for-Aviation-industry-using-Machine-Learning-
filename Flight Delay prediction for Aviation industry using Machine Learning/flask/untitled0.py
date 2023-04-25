36de64a7738fe7d74e4b38ac0fdebf2f16fefcf7
sdayanasdayana9 <128214686+sdayanasdayana9@users.noreply.github.com>
2023-04-25 05:35:04 +0530

    Add files via upload

---
+++ Flight Delay prediction for Aviation industry using Machine Learning/flask/untitled0.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 24 19:39:57 2023
+
+@author: Star World
+"""
+
+def predict():
+    name=requst.form['name']
+    month=request.form['month']
+    dayofmonth=request.form['dayofmonth']
+    dayofweek=request.form['dayofweek']
+    origin=request.form['origin']
+    if(origin=="msp"):
+        origin1,origin2,origin3,origin4,origin5=0,0,0,0,1
+    if(origin=="dtw"):
+        origin1,origin2,origin3,origin4,origin5=1,0,0,0,0
+    if(origin=="jfk"):
+        origin1,origin2,origin3,origin4,origin5=0,0,1,0,0
+    if(origin=="sea"):
+        origin1,origin2,origin3,origin4,origin5= 0,1,0,0,0    
+    if(origin=="alt"):
+        origin1,origin2,origin3,origin4,origin5=0,0,0,1,0   
+
+destination=request.form['destination']
+if(destination=="msp"):
+  destination1,destination2,destination3,destination4,destination5=0,0,0,0,1
+if(destination=="dtw"):
+  destination1,destination2,destination3,destination4,destination5=1,0,0,0,0
+if(destination=="jfk"):
+  destination1,destination2,destination3,destination4,destination5=0,0,1,0,0
+if(destination=="sea"):
+  destination1,destination2,destination3,destination4,destination5=0,1,0,0,0
+if(destination=="alt"):
+  destination1,destination2,destination3,destination4,destination5=0,0,0,1,0 
+dept=request.form['dept']
+arrtime=request.form['arrtime']
+actdept=request.form['actdept']
+dept15=int(dept)-int(actdept)
+total=[(name,month,dayofmonth,dayofweek,origin1,origin2,origin3,origin4,origin5,destination1,destination2,destination3,destination4,destination5,i)]
+#print(total)
+y_pred=model.predict(todal)
+print(y_pred)
+if(y_pred==[0.1]):
+  ans="The Flight will be on time"
+else:
+  ans="The Flight will be on delayed"
+return render_template("index.html",showcase=ans)
